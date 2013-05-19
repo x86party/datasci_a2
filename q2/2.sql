be36ac5914f0375d56aa60870bc0d63d9ae8f09b
Tom Ravenscroft <tom.ravenscroft@saitech.com.au>
2013-05-19 16:30:09 +1000

    Added SQL query for matrix multiplication.

---
+++ q2/2.sql
@@ -0,0 +1,10 @@
+/*
+	Matrix multiplication for sparse matrix representation.
+*/
+
+select distinct A.row_num,B.col_num,sum(A.value*B.value)
+from A,B 
+where
+A.col_num = B.row_num
+group by A.row_num, B.col_num
+;
